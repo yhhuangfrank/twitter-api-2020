53915d4cecae7eab074ab3c238b35668e2326339
wego11ya <wego11ya@gmail.com>
2023-04-05 12:01:22 +0800

    feat: init socket.io

--- app.js
+++ app.js
@@ -1,27 +1,33 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
-const express = require('express')
-const cors = require('cors')
-const passport = require('./config/passport')
-const helpers = require('./helpers/auth-helper')
-const routes = require('./routes/index')
+const express = require("express");
+const cors = require("cors");
+const passport = require("./config/passport");
+const helpers = require("./helpers/auth-helper");
+const routes = require("./routes/index");
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+// Pass Server to socket.io instead of expresss application function.
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
 
+const port = process.env.PORT || 3000;
 
 // use helpers.getUser(req) to replace req.user
 // function authenticated(req, res, next){
 //   // passport.authenticate('jwt', { ses...
 // };
-app.use(cors())
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(passport.initialize())
-app.use(routes)
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(passport.initialize());
+app.use(routes);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// Pass Server to socket.io instead of expresss application function.
+server.listen(port, () =>
+  console.log(`Example app listening on port ${port}!`)
+);
 
-module.exports = app
+module.exports = app;
