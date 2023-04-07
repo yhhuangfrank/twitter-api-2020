a07b5208a6d06998dba8656b061447860fef5796
FrankHuang <frankcool321@gmail.com>
2023-04-07 22:19:13 +0800

    feat: modify currentUsers by sequence

--- controllers/websocket-controller.js
+++ controllers/websocket-controller.js
@@ -1,4 +1,4 @@
-const currentUsers = {}; // - 儲存目前登入使用者
+let currentUsers = {}; // - 儲存目前登入使用者
 const { Public_Message } = require("../models");
 const { getCurrentUserList } = require("../helpers/data-helper");
 module.exports = (io) => {
@@ -7,12 +7,12 @@ module.exports = (io) => {
     /*監聽事件，這邊的send_message是從前端發送socket.emit('send_message', options)*/
     socket.on("send_message", async (data) => {
       console.log(data); // - 傳入使用者發送的訊息與個人資料
-      
+
       //- 透過伺服器廣播給自己以外所有人，前端會用socket.on('receive_message')監聽後，再渲染畫面
       socket.broadcast.emit("receive_message", data);
       const { id } = data.author;
       const { time, message } = data;
-      
+
       // - 將訊息存到 DB
       await Public_Message.create({
         UserId: id,
@@ -23,14 +23,17 @@ module.exports = (io) => {
 
     // - user 加入聊天室
     socket.on("join_chat", (user) => {
-      console.log("有人加入了")
+      console.log("有人加入了");
       if (!user || !user.id || currentUsers[user.id]) return;
 
       // - 儲存至 currentUsers
-      currentUsers[user.id] = user;
+      const newCurrentUsers = { ...currentUsers };
+      newCurrentUsers[user.id] = user // - 加入新成員
+      currentUsers = { ...newCurrentUsers };
+      
       const currentUsersList = getCurrentUserList(currentUsers);
-      console.log(currentUsersList)
-      console.log(`目前有${currentUsersList.length}位在聊天室`)
+      console.log(currentUsersList);
+      console.log(`目前有${currentUsersList.length}位在聊天室`);
 
       // - 回傳目前在線上的人列表
       io.emit("user_join", currentUsersList);
@@ -38,16 +41,16 @@ module.exports = (io) => {
 
     // - user 離開聊天室
     socket.on("leave_chat", (user) => {
-      console.log("有人離開了")
+      console.log("有人離開了");
       if (!user || !user.id || !currentUsers[user.id]) return;
 
-      delete currentUsers[user.id]
+      delete currentUsers[user.id];
       const currentUsersList = getCurrentUserList(currentUsers);
 
-      console.log(`目前有${currentUsersList.length}位在聊天室`)
-    })
+      console.log(`目前有${currentUsersList.length}位在聊天室`);
+    });
 
     // - 離開 socket 連線
-    socket.on("disconnect", () => console.log("user Diconnected", socket.id))
+    socket.on("disconnect", () => console.log("user Diconnected", socket.id));
   });
 };
