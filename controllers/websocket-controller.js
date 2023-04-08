d195df7e78b8e737682c6f41ee34309f22201dbc
FrankHuang <frankcool321@gmail.com>
2023-04-08 22:04:12 +0800

    feat: add user_leave event when user leave chat

--- controllers/websocket-controller.js
+++ controllers/websocket-controller.js
@@ -6,8 +6,6 @@ module.exports = (io) => {
   io.on("connection", function (socket) {
     /*監聽事件，這邊的send_message是從前端發送socket.emit('send_message', options)*/
     socket.on("send_message", async (data) => {
-      console.log(data); // - 傳入使用者發送的訊息與個人資料
-
       //- 透過伺服器廣播給自己以外所有人，前端會用socket.on('receive_message')監聽後，再渲染畫面
       socket.broadcast.emit("receive_message", data);
       const { id } = data.author;
@@ -23,7 +21,6 @@ module.exports = (io) => {
 
     // - user 加入聊天室
     socket.on("join_chat", (data) => {
-      console.log("有人加入了");
       const { user, time } = data;
       if (!user || !user.id || currentUsers[user.id]) return;
 
@@ -32,8 +29,6 @@ module.exports = (io) => {
       currentUsers[user.id] = user; // - 加入新成員
 
       const currentUsersList = getCurrentUserList(currentUsers);
-      console.log(currentUsersList);
-      console.log(`目前有${currentUsersList.length}位在聊天室`);
 
       // - 回傳目前在線上的人列表
       io.emit("user_join", currentUsersList);
@@ -41,13 +36,13 @@ module.exports = (io) => {
 
     // - user 離開聊天室
     socket.on("leave_chat", (user) => {
-      console.log("有人離開了");
       if (!user || !user.id || !currentUsers[user.id]) return;
 
       delete currentUsers[user.id];
       const currentUsersList = getCurrentUserList(currentUsers);
-      console.log(currentUsersList);
-      console.log(`目前有${currentUsersList.length}位在聊天室`);
+
+      // - 回傳目前在線上的人列表，離線使用者
+      io.emit("user_leave", { currentUsersList, user });
     });
 
     // - 離開 socket 連線
